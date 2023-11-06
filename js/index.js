0a6b2f9120793eef352783f1e208db9178aafbb2
tingyou55b <tingyou55b@gmail.com>
2023-11-06 11:49:49 +0800

    Revert "merge dog"
    This reverts commit 75c202019e7325e920656cf556b31bc9cfcac051, reversing
    changes made to 89a652dd9e8e11422f62ddee13267c81275fe82d.

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ function myFirstFun() {
     console.log('white cat');
     console.log('black cat');
     console.log('blue cat');
-    console.log('apple pie');
     console.log('black dog');
     console.log('white dog');
     console.log('puppy');
